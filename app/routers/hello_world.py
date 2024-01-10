bd51677c668ab0f8037bf07e332ac21b6be66f7a
railnlays <juliomdp92@gmail.com>
2024-01-11 00:11:52 +0100

    cd run DO DEV

--- app/routers/hello_world.py
+++ app/routers/hello_world.py
@@ -1,5 +1,12 @@
 from sklearn.ensemble import RandomForestRegressor
+from fastapi import APIRouter
 import pickle
 
 with open("model.pkl", mode= "rb") as file:
-    m: list[str] = pickle.load(file)
+    model: RandomForestRegressor = pickle.load(file)
+
+router = APIRouter(prefix="/api/v1/hello-word", tags=["hello-world"])
+
+@router.get("")
+async def get_hello_word() -> str:
+    return "Hello World!"
