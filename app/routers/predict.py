5bf365efba82a62ec0749c2d70a5b34f851358d9
railnlays <juliomdp92@gmail.com>
2024-01-11 11:54:39 +0100

    cd run DO DEV

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -1,11 +1,12 @@
 from sklearn.ensemble import RandomForestRegressor
 from fastapi import APIRouter, Body
 import pickle
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+import numpy as np
 
 
 class Features(BaseModel):
-    day_binary: int
+    day_binary: int = Field(default=20)
     month_binary: int
     year_binary: int
     interval: int
@@ -24,4 +25,19 @@ router = APIRouter(prefix="/api/v1/predict", tags=["predict"])
 
 @router.post("/predict")
 async def post_predict(features: Features = Body()) -> float:
-    return 4.0
+    input_data = np.array([[
+        features.day_binary,
+        features.month_binary,
+        features.year_binary,
+        features.interval,
+        features.orientation,
+        features.med_azimut_angle,
+        features.med_elevation_angle,
+        features.des_azimut_angle,
+        features.des_elevation_angle,
+    ]])
+
+    prediction = model.predict(input_data)
+
+    return float(prediction[0])
+
